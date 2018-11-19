53f020a423ab0f833745afa81595c12614a3a530
Bruno Celeste <bruno.celeste@gmail.com>
2018-11-19 14:40:03 +0100

    Move Coconut_Job class into Coconut.php

--- src/Coconut.php
+++ src/Coconut.php
@@ -104,3 +104,14 @@ class Coconut {
     return join("\n", $new_conf);
   }
 }
+
+class Coconut_Job {
+    public static function create($options=array()) {
+        $api_key = null;
+        if(isset($options['api_key'])) {
+            $api_key = $options['api_key'];
+        }
+
+        return Coconut::submit(Coconut::config($options), $api_key);
+    }
+}
