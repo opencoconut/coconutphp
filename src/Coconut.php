c588c62d2765f23194c162530802308e3b19e292
Bruno Celeste <bruno.celeste@gmail.com>
2019-06-19 11:33:05 +0200

    Code refactor
    Implemented method getApiKey to retrieve API key before each request.

--- src/Coconut.php
+++ src/Coconut.php
@@ -146,38 +146,35 @@ class Coconut {
 
 class Coconut_Job {
     public static function create($options=array()) {
-        $api_key = null;
-        if(isset($options['api_key'])) {
-            $api_key = $options['api_key'];
-        }
+        $api_key = self::getApiKey($options);
 
         return Coconut::submit(Coconut::config($options), $api_key);
     }
 
     public static function get($jid, $options=array()) {
-        $api_key = null;
-        if(isset($options['api_key'])) {
-            $api_key = $options['api_key'];
-        }
+        $api_key = self::getApiKey($options);
 
         return Coconut::get('/v1/jobs/' . $jid, $api_key);
     }
 
     public static function getAllMetadata($jid, $options=array()) {
-        $api_key = null;
-        if(isset($options['api_key'])) {
-            $api_key = $options['api_key'];
-        }
+        $api_key = self::getApiKey($options);
 
         return Coconut::get('/v1/metadata/jobs/' . $jid, $api_key);
     }
 
     public static function getMetadataFor($jid, $source_or_output, $options=array()) {
-        $api_key = null;
-        if(isset($options['api_key'])) {
-            $api_key = $options['api_key'];
-        }
+        $api_key = self::getApiKey($options);
 
         return Coconut::get('/v1/metadata/jobs/' . $jid . '/' . $source_or_output, $api_key);
+    }
+
+
+    private static function getApiKey($options=array()) {
+      $api_key = null;
+      if(isset($options['api_key'])) {
+          $api_key = $options['api_key'];
+      }
+      return $api_key;
     }
 }
